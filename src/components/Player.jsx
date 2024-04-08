0eea9396c8cc8b5bb09e5e3821f084bd488fe387
Ragul <ragulraja2808@gmail.com>
2024-04-08 17:19:58 +0530

    Update App and Player components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,24 +2,35 @@ import { useState } from "react"
 
 
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
-
+        setIsEditing((editing) => !editing);; // => sechduels a state update to true
+       
     }
-    let playerName =  <span className="player-name">{name}</span>;
-    // let btncaption = 'Edit'
+    function handlechange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = (
+        <span className="player-name">
+            <input type="text" value={playerName} onChange={handlechange} />
+        </span>
+    );
+    
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
-        // let btncaption = 'save'
+        editablePlayerName = (
+          <input type="text" required value={playerName} />
+          // let btncaption = 'save'
+        );
     }
     return (
         <li>
            <span className="player">
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
             <button onClick={handleEditClick}>{isEditing ? 'save' : 'Edit' }</button>
            </span>
