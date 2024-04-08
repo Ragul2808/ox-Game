5562a1537f0a930621eb5a38058e6c5d4a2d6445
Ragul <ragulraja2808@gmail.com>
2024-04-08 15:09:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li></li>
+          <li></li>
+        </ol>
+
+        <div id="board">
+          GAME BOARD
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
